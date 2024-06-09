082a9028b2e90d89fb77ca3be88efc727b46323c
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-06-09 20:18:43 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    first_num = 0
+    second_num = 1
+    for _ in range(length):
+        fib_sequence.append(first_num)
+        temp = first_num + second_num
+        first_num = second_num
+        second_num = temp
+    print(fib_sequence)
+
+print_fibonacci(9)
+    
